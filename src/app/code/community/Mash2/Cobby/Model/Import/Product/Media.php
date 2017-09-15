98ee725b694d6d4dc04a84085ccabee9975490c2
slavko dick <slavko.dick@mash2.com>
2017-09-15 11:52:06 +0200

    removed comment for debugging

--- src/app/code/community/Mash2/Cobby/Model/Import/Product/Media.php
+++ src/app/code/community/Mash2/Cobby/Model/Import/Product/Media.php
@@ -221,11 +221,11 @@ class Mash2_Cobby_Model_Import_Product_Media extends Mash2_Cobby_Model_Import_Pr
                     }
 
                     $attribute = $this->_resource->getAttribute($key);
-                    // here happens the magic, if attribute not found, ..., !!!BOOM!!
-                    //comment only for debug purposes
+
                     if (!$attribute) {
                         continue;
                     }
+
                     $attrTable = $attribute->getBackend()->getTable();
                     $attrId = $attribute->getId();
                     $attributesData[$attrTable][$productId][$attrId][$storeId] = $file;
